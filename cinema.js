982dbfe8c97c3676e31be0e7619a0d9d5888ba03
LucasMezzaJalaUniversity <Lucas.Mezza@jala.university>
2025-05-19 10:56:31 -0300

    createCinema & showSeatStatus methods

--- cinema.js
+++ cinema.js
@@ -22,11 +22,38 @@ class Cinema {
     this.cinemaSeats = this.createCinema()
   }
 
-  createCinema() {}
+  createCinema() {
+    let arrayRows = new Array(this.rows)
+    for (let i = 0; i < this.rows; i++) {
+      arrayRows[i] = new Array(this.rowsSeats)
+      for (let j = 0; j < this.rowsSeats; j++) {
+        arrayRows[i][j] = new Seat(i + 1, j + 1);
+      }
+    }
 
-  showSeatStatus() {}
+    return arrayRows;
+  }
+
+  showSeatStatus() {
+    console.log(" ")
+    console.log("====== SCREEN ======")
+    for (let i = 0; i < this.rows; i++) {
+      let row = "ROW " + (i + 1) + " ";
+      for (let j = 0; j < this.rowsSeats; j++) {
+        row += this.cinemaSeats[i][j].reserved ? "R " : "A ";
+      }
+      console.log(row)
+    }
+    console.log("      ")
+    console.log("A: Available, R: Reserved")
+    console.log(" ")
+  }
 
   reserveSeat(row, seat) {}
 }
+
+const cinema = new Cinema(5, 10)
+
+cinema.showSeatStatus()
 
 
