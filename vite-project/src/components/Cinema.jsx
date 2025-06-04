731f509202c9ed3b73cd3f2a39bf0d74b0f60209
LucasMezzaJalaUniversity <Lucas.Mezza@jala.university>
2025-06-04 20:01:37 -0300

    return Cinema structure and use Cinema component in the App

--- vite-project/src/components/Cinema.jsx
+++ vite-project/src/components/Cinema.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 export const Cinema = ({ row, rowsSeats }) => {
   const [cinemaSeats, setCinemaSeats] = useState(
     Array.from({ length: row }, (row, rowIndex) => {
-      Array.from({ length: rowsSeats }, (seat, seatIndex) => {
+      return Array.from({ length: rowsSeats }, (seat, seatIndex) => {
         return {
           row: rowIndex + 1,
           seat: seatIndex + 1,
@@ -13,5 +13,25 @@ export const Cinema = ({ row, rowsSeats }) => {
     })
   )
 
-  return (<></>)
+  console.log(cinemaSeats)
+
+  return (
+    <section>
+      <h1>========= SCREEN =========</h1>
+      <ul>
+        {cinemaSeats.map((elm, idx) => (
+          <li key={idx}>
+            <em>Row {idx + 1}:</em>
+            <ul>
+              {elm.map((seat, seatIdx) => (
+                <li key={seatIdx}>
+                  <em>{seat.reserved ? 'R' : 'A'}</em>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
 }
