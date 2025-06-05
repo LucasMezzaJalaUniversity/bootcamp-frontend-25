8eebe977e540994aa7e15cb17c604ffd8ed8ce54
LucasMezzaJalaUniversity <Lucas.Mezza@jala.university>
2025-06-05 19:48:17 -0300

    create html structure and css styles

--- vite-project/src/components/Seat.jsx
+++
@@ -1,9 +0,0 @@
-import './Seat.css'
-
-export const Seat = ({seat, onClick = () => {} }) => {
-  return (
-    <li key={seat.seat} className="seat">
-      <button onClick={onClick} className={`${seat.reserved ? 'reserved' : ''}`}>{seat.reserved ? 'R' : 'A'}</button>
-    </li>
-  )
-}
