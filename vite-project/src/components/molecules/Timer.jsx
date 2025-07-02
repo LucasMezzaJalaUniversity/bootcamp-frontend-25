0c2b0bf23705a2f68567f8839872672050517ebc
LucasMezzaJalaUniversity <Lucas.Mezza@jala.university>
2025-07-02 17:47:37 -0300

    timer functions

--- vite-project/src/components/molecules/Timer.jsx
+++ vite-project/src/components/molecules/Timer.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import { Button } from "../atoms/Button"
 
 export const Timer = () => {
+  const [timer, setTimer] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const handleStart = () => {
+    isRunning(true)
+  }
+
+  const handleStop = () => {
+    isRunning(false)
+  }
+
+  const handleReset = () => {
+    setTimer(0);
+    setIsRunning(false);
+  }
+
   return (
     <div>
       <h1>Timer</h1>
       <span><span>0</span> mins <span>0</span> secs</span>
       <div>
-        <Button>Start</Button>
-        <Button>Stop</Button>
-        <Button>Reset</Button>
+        <Button onClick={handleStart}>Start</Button>
+        <Button onClick={handleStop}>Stop</Button>
+        <Button onClick={handleReset}>Reset</Button>
       </div>
     </div>
   )
